01dce4ba8504b3af739e5a9019d0e3747dfc68c9
Aldo Bucchi <aldo.bucchi@gmail.com>
2020-04-29 14:33:36 -0700

    format

--- redwood-ts-1/src-gen/global-gql.ts
+++ redwood-ts-1/src-gen/global-gql.ts
@@ -1,4 +1,3 @@
-
 // this file doesn't need to be generated dynamically
 // these declarations could be provided by @redwoodjs/web, for example
-declare function gql(...args:any[])
+declare function gql(...args: any[])
